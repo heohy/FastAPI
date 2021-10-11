d0ec327d2b2a68d3cde573e86854a42e02cc3e2a
hyeokjinheo <boksilboksil54@naver.com>
2021-10-11 17:56:58 +0900

    인덱스 페이지 생성 20211011

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ templates = Jinja2Templates(directory="templates")
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse("item.html", {"request": request, "id": id})
 
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "id": 'Main'})
+
+
+
 # ref. https://fastapi.tiangolo.com/advanced/templates/
 # ref. https://fastapi.tiangolo.com/tutorial/
 # ref. uvicorn main:app --reload
